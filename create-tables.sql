b1c20235952368047def10f68b885b9490e3c730
Macsen Casaus <macsencasaus@gmail.com>
2025-02-14 10:14:54 -0600

    create tables script & join db script

---
+++ create-tables.sql
@@ -0,0 +1,87 @@
+CREATE TABLE Customer (
+    id              SERIAL NOT NULL,
+    phone_number    VARCHAR(32) NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TYPE payment_type AS ENUM('cash', 'card', 'gift_card', 'check');
+
+CREATE TABLE Transaction (
+    id              SERIAL NOT NULL,
+    payment_type    payment_type NOT NULL,
+    cc_digits       VARCHAR(4),
+    time            TIMESTAMP NOT NULL,
+    price           INT NOT NULL,
+    tip             INT NOT NULL,
+    customer_id     INT,
+
+    PRIMARY KEY (id),
+
+    CONSTRAINT FK_Customer_Id FOREIGN KEY (customer_id)
+    REFERENCES Customer(id)
+);
+
+CREATE TYPE product_type AS ENUM (
+    'milk_tea',
+    'fruit_tea',
+    'brewed_tea',
+    'fresh_milk',
+    'ice_blended',
+    'tea_mojito',
+    'creama',
+    'ice_cream',
+    'misc',
+    'topping',
+    'special_item'
+);
+
+CREATE TABLE Product (
+    id              SERIAL NOT NULL,
+    product_type    product_type NOT NULL,
+    name            VARCHAR(256) NOT NULL,
+    price           INT NOT NULL,
+    inventory       INT NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Transaction_Item (
+    id              SERIAL NOT NULL,
+    transaction_id  INT NOT NULL,
+    product_id      INT NOT NULL,
+    quantity        INT NOT NULL,
+    subtotal        INT NOT NULL,
+
+    PRIMARY KEY (id),
+
+    CONSTRAINT FK_Transaction_Id FOREIGN KEY (transaction_id)
+    REFERENCES Transaction(id),
+
+    CONSTRAINT FK_Product_Id FOREIGN KEY (product_id)
+    REFERENCES Product(id)
+);
+
+CREATE TABLE Employee (
+    id          SERIAL NOT NULL,
+    name        VARCHAR(256) NOT NULL,
+    password    VARCHAR(256) NOT NULL,
+    admin       BOOL NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TYPE supply_type AS ENUM (
+    'cup',
+    'straw',
+    'paper'
+);
+
+CREATE TABLE Supply (
+    id          SERIAL NOT NULL,
+    name        VARCHAR(256) NOT NULL,
+    supply_type supply_type NOT NULL,
+    inventory   INT NOT NULL,
+
+    PRIMARY KEY (id)
+);
---
+++ join-db.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+psql -h csce-315-db.engr.tamu.edu -U team_61 -d team_61_db
