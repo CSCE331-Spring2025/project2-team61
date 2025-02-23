74f13b8b6b814b33baf1fc6647b116b3891b88db
LukeConran <stlsoccerdude@gmail.com>
2025-02-22 18:08:39 -0600

    Added Styling, second frame, and main class

--- gui/LoginScreen.java
+++ gui/LoginScreen.java
@@ -1,41 +1,41 @@
 import javax.swing.*;
 import java.awt.Font;
 
-class LoginScreen {
-    static final int windowWidth = 1200;
-    static final int windowHeight = 800;
-
+public class LoginScreen extends JFrame {
     static final int textFieldWidth = 220;
-    static final int testFieldHeight = 50;
-
-    static final int loginButtonHeight = windowHeight / 2;
+    static final int textFieldHeight = 50;
 
     static final Font textFieldFont = new Font("Arial", Font.PLAIN, 24);
     static final Font loginButtonFont = new Font("Arial", Font.BOLD, 32);
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Login");
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    public LoginScreen() {
+        super("Login");
+        FrameStyle.StyleFrame(this);
+        initializeComponents();
+    }
+
+    private void initializeComponents() {
+        setLayout(null);
+        int windowWidth = FrameStyle.screenWidth;
+        int windowHeight = FrameStyle.screenHeight;
+        int loginButtonYPosition = windowHeight / 2;
 
         JTextField usernameField = new JTextField(50);
         usernameField.setFont(textFieldFont);
-        usernameField.setBounds(windowWidth / 2 - textFieldWidth / 2, loginButtonHeight - 2 * testFieldHeight, 220, 50);
+        usernameField.setBounds(windowWidth / 2 - textFieldWidth / 2, loginButtonYPosition - 2 * textFieldHeight, textFieldWidth, textFieldHeight);
 
         JPasswordField passwordField = new JPasswordField(50);
         passwordField.setFont(textFieldFont);
-        passwordField.setBounds(windowWidth / 2 - textFieldWidth / 2, loginButtonHeight - testFieldHeight, 220, 50);
+        passwordField.setBounds(windowWidth / 2 - textFieldWidth / 2, loginButtonYPosition - textFieldHeight, textFieldWidth, textFieldHeight);
 
         JButton loginButton = new JButton("Login");
         loginButton.setFont(loginButtonFont);
-        loginButton.setBounds(windowWidth / 2 - textFieldWidth / 2, loginButtonHeight, 220, 50);
+        loginButton.setBounds(windowWidth / 2 - textFieldWidth / 2, loginButtonYPosition, textFieldWidth, textFieldHeight);
 
-        frame.add(usernameField);
-        frame.add(passwordField);
-        frame.add(loginButton);
+        add(usernameField);
+        add(passwordField);
+        add(loginButton);
 
-        frame.setSize(windowWidth, windowHeight);
-
-        frame.setLayout(null);
-        frame.setVisible(true);
+        setSize(windowWidth, windowHeight);
     }
 }
