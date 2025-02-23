5173f167a9a8770655cab4275ca61993f772e4fb
Surada Suwansathit <suwansathit.s@gmail.com>
2025-02-23 17:11:50 +0000

    add imports

--- gui/ManagerPage.java
+++ gui/ManagerPage.java
@@ -1,3 +1,10 @@
-public class ManagerPage {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ManagerPage extends JFrame {
     
 }
