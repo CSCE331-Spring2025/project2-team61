20de6b5769570ce4f75149905b6cc71ad0debed8
Macsen Casaus <macsencasaus@gmail.com>
2025-02-25 14:10:53 -0600

    add product entry class + fix makefile clean

--- gui/ItemList.java
+++ gui/ItemList.java
@@ -4,18 +4,6 @@ import javax.swing.*;
 import java.util.HashMap;
 import java.util.function.Consumer;
 
-class ProductEntry {
-    public int id;
-    public String name;
-    public double price;
-
-    public ProductEntry(int id, String name, double price) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-    }
-}
-
 public class ItemList extends JFrame {
     // private DefaultListModel<String> listModel;
     // private JList<String> itemList;
---
+++ gui/ProductEntry.java
@@ -0,0 +1,10 @@
+public class ProductEntry {
+    public int id;
+    public String name;
+    public double price;
+    public ProductEntry(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
