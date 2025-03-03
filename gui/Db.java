c739d55c808f9e17d22fc41c1e4a3b9d579d0314
Macsen Casaus <macsencasaus@gmail.com>
2025-03-03 09:48:29 -0600

    javadoc db file

--- gui/Db.java
+++ gui/Db.java
@@ -1,14 +1,27 @@
 import java.sql.*;
 
+/**
+ * This class handles database connectivity and queries using PostgreSQL.
+ * It establishes a connection to a specified database and allows querying.
+ * 
+ * @author Luke Conran
+ * @author Kamryn Vogel
+ * @author Christian Fadal
+ * @author Macsen Casaus
+ * @author Surada Suwansathit
+ */
 public class Db {
-    String databaseName;
-    String databaseUser;
-    String databasePassword;
+    private String databaseName;
+    private String databaseUser;
+    private String databasePassword;
+    private Connection conn;
 
-    Connection conn;
+    private static final String databaseURLFormat = "jdbc:postgresql://csce-315-db.engr.tamu.edu/%s";
 
-    static final String databaseURLFormat = "jdbc:postgresql://csce-315-db.engr.tamu.edu/%s";
-
+    /**
+     * Default constructor for the database connection.
+     * Initializes connection with predefined database credentials.
+     */
     public Db() {
         databaseName = "team_61_db";
         databaseUser = "team_61";
@@ -17,6 +30,13 @@ public class Db {
         connect();
     }
 
+    /**
+     * Parameterized constructor for establishing a database connection.
+     * 
+     * @param databaseName     The name of the database.
+     * @param databaseUser     The username for database authentication.
+     * @param databasePassword The password for database authentication.
+     */
     public Db(String databaseName, String databaseUser, String databasePassword) {
         this.databaseName = databaseName;
         this.databaseUser = databaseUser;
@@ -25,6 +45,13 @@ public class Db {
         connect();
     }
 
+    /**
+     * Executes a SQL query on the connected database.
+     * 
+     * @param fmt  The SQL query format string.
+     * @param args The arguments to format into the SQL query.
+     * @return A {@code ResultSet} containing the results of the query, or {@code null} if an error occurs.
+     */
     public ResultSet query(String fmt, Object... args) {
         String sqlStatement = String.format(fmt, args);
         try {
@@ -37,6 +64,10 @@ public class Db {
         }
     }
 
+    /**
+     * Establishes a connection to the PostgreSQL database.
+     * Uses the PostgreSQL JDBC driver to connect using the provided credentials.
+     */
     private void connect() {
         String databaseURL = String.format(databaseURLFormat, databaseName);
 
@@ -51,3 +82,4 @@ public class Db {
         // System.out.println("DB Connection Established Successfully!");
     }
 }
+.
